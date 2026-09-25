85eb29f062b2e5640702080f2ae3e4c6019d56ba
A Hari Kiran <harikiran.dev.schooldb@gmail.com>
2026-09-25 17:14:52 +0530

    Polish section setup layout

--- src/app/[schoolSlug]/(app)/setup/sections/page.tsx
+++ src/app/[schoolSlug]/(app)/setup/sections/page.tsx
@@ -323,7 +323,7 @@ export default function SetupSectionsPage() {
   /* ------------------------------------------------------------------ */
 
   return (
-    <div className="space-y-8 pb-12">
+    <div className="space-y-6 p-4 pb-12 sm:p-6">
       <PageHeader
         eyebrow="School Setup"
         title="Sections"
