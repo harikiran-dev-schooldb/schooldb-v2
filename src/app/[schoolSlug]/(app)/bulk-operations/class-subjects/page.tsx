b3b94655ae671efd343ecb89ef1c89fcff225759
A Hari Kiran <harikiran.dev.schooldb@gmail.com>
2026-09-25 16:44:04 +0530

    Polish bulk class subjects bulk import layout

--- src/app/[schoolSlug]/(app)/bulk-operations/class-subjects/page.tsx
+++ src/app/[schoolSlug]/(app)/bulk-operations/class-subjects/page.tsx
@@ -212,7 +212,7 @@ export default function BulkClassSubjectsPage() {
   const hasFile = Boolean(fileName) && !fileError;
 
   return (
-    <div className="space-y-8 pb-12">
+    <div className="space-y-6 p-4 pb-12 sm:p-6">
       <PageHeader
         eyebrow="Bulk Operations"
         title="Bulk Class Subjects"
