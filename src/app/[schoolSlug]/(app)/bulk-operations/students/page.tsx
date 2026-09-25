7004ca6d51d96cf29eccfcb415f1fa4777aac9e7
A Hari Kiran <harikiran.dev.schooldb@gmail.com>
2026-09-25 16:44:57 +0530

    Polish bulk operations responsive layout

--- src/app/[schoolSlug]/(app)/bulk-operations/students/page.tsx
+++ src/app/[schoolSlug]/(app)/bulk-operations/students/page.tsx
@@ -386,7 +386,7 @@ export default function BulkStudentsPage() {
   const hasFileResult = Boolean(fileName) && !fileError;
 
   return (
-    <div className="space-y-8 pb-12">
+    <div className="space-y-6 p-4 pb-12 sm:p-6">
       <PageHeader
         eyebrow="Bulk Operations"
         title="Bulk Students"
