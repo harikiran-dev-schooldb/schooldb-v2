b0c1635777a7fa5d1490c1cde57f6febe6a24e1f
A Hari Kiran <harikiran.dev.schooldb@gmail.com>
2026-09-25 16:23:43 +0530

    Polish teacher directory toolbar

--- src/features/teachers/components/TeacherTable.tsx
+++ src/features/teachers/components/TeacherTable.tsx
@@ -38,9 +38,9 @@ export function TeacherTable() {
       onPageChange={setPage}
       toolbar={
         <div className="flex flex-col gap-3">
-          <div className="flex justify-end">
-            <Button asChild className="rounded-xl bg-emerald-600 text-white hover:bg-emerald-700">
-              <a href={exportHref}><Download className="mr-2 size-4" />Export Excel</a>
+          <div className="flex justify-end px-1 pt-1">
+            <Button asChild className="w-full rounded-xl bg-emerald-600 text-white hover:bg-emerald-700 sm:w-auto">
+              <a href={exportHref} className="inline-flex items-center"><Download className="mr-2 size-4" />Export Excel</a>
             </Button>
           </div>
           <TeacherToolbar search={search} onSearch={setSearch} />
